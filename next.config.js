f081a719f32e6b0835980fa4685a7655b9f543ae
Mohammadreza Sharifi <mrsharifi.cs@gmail.com>
2023-07-19 19:11:50 +0300

    blog page - posts/css

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   images: {
     domains: [
       "images.pexels.com",
+      "https://miro.medium.com",
       "img.freepik.com", // if we have external images, we should define domains here
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 };
